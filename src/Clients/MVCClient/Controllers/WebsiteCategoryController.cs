1732bd22d36ff9296efd65f8272f7123da3dd5b1
Kevin Andres Vasquez Benavides <kevinavbdev@outlook.com>
2021-10-06 08:49:41 -0600

    Create website category feature has been added.

---
+++ src/Clients/MVCClient/Controllers/WebsiteCategoryController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCClient.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCClient.Controllers
+{
+    [Controller]
+    public class WebsiteCategoryController : Controller
+    {
+        [HttpGet("/v1/WebsiteCategory")]
+        public async Task<IActionResult> CreateWebsiteCategory()
+        {
+            return View();
+        }
+
+        [HttpPost("/v1/WebsiteCategory")]
+        public async Task<IActionResult> CreateWebsiteCategory(CreateWebsiteCategoryModel model)
+        {
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch (Exception)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
