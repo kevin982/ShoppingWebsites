a31b67cd9e7ffd1cc1da25105c13ebc45de60422
Kevin Andres Vasquez Benavides <kevinavbdev@outlook.com>
2021-10-18 09:31:40 -0600

    Create website category feature has been added.

--- src/Clients/MVCClient/Controllers/WebsiteCategoryController.cs
+++ src/Clients/MVCClient/Controllers/WebsiteCategoryController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace MVCClient.Controllers
@@ -27,6 +28,34 @@ namespace MVCClient.Controllers
             catch (Exception)
             {
                 throw new NotImplementedException();
+            }
+        }
+
+        [HttpGet("/v1/WebsiteCategories")]
+        public async Task<string> GetAllCategories()
+        {
+            try
+            {
+                //Here I must implement the actual code
+
+                
+                var content = new[]
+                {
+                   new { categoryId = new Guid("bec567d7-0009-4762-85f6-3b967602dfa2"), categoryName = "Clothes" },
+                   new { categoryId = new Guid("5aa4529f-2467-410b-82c0-5d6ba69d8490"), categoryName = "Sport" },
+                   new { categoryId = new Guid("2351cf51-0795-4ba8-936e-cc959f495e43"), categoryName = "Shoes" }
+                };
+
+                var result = new { content = content, statusCode = 200, succeeded = true, title = "The website categories were reached!"};
+
+                return JsonSerializer.Serialize(result);    
+
+
+
+            }
+            catch (Exception)
+            {
+                throw;
             }
         }
     }
